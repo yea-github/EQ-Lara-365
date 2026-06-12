1d2021c9d18ada1b11e338857e6c4e0981692efb
Muhammad Yeasin <yea.dev.de@gmail.com>
2026-06-12 20:48:40 +0200

    Laravel APP, React APP, Wellcome page

---
+++ Front-End/React/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white shadow-lg rounded-lg p-8">
+        <h1 className="text-4xl font-bold text-blue-600">
+          ERP Dashboard
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          ReactJS + TailwindCSS is working!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
